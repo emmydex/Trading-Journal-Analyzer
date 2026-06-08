68b8bde71ec48ac5a2cb04d90d5d64510326e5e8
oluwafemi 🍀 <39830671+emmydex@users.noreply.github.com>
2026-06-08 19:12:21 -0400

    handling a zero division error if there are no trades

--- version 1.py
+++ version 1.py
@@ -21,13 +21,24 @@ total_trades = len(trades)
 # for empty file error handling
 if total_trades == 0:
     print("no trades found")
+    exit()
+
+
+
+
 
 # naming variables
 winning_trades = 0 
 losing_trades = 0
 total_profit = 0
 
-
+# handling a zero division error if there are no trades
+if total_trades > 0:
+     win_rate = (winning_trades / total_trades)* 100
+     average_profit = total_profit / total_trades
+else:
+     win_rate = 0
+     average_profit = 0
 
     # coounting winning and losing trades
 for trade in trades:
@@ -51,6 +62,6 @@ print("======================\n")
 print("Total trades :", total_trades)
 print("Winning Trades:" , winning_trades)
 print("Losing Trades:", losing_trades)
-print("total profit: $" ,total_profit)
-print(f"win rate: ${win_rate:.1f}%")
+print(f"total profit: ${total_profit:.2f}")
+print(f"win rate: {win_rate:.1f}%")
 print(f"Average Profit: ${average_profit:.2f}")
