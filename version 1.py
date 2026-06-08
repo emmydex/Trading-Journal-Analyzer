024d2ba10634c6e6b30f91c6122d1009832cb84d
oluwafemi 🍀 <39830671+emmydex@users.noreply.github.com>
2026-06-08 14:52:30 -0400

    comments

--- version 1.py
+++ version 1.py
@@ -9,18 +9,21 @@ with open("journal.csv" , "r") as file:
 trades = []
 
 # counting of total trades
-
 total_trades = len(trades)
 
 winning_trades = 0 
 losing_trades = 0
 total_profit = 0
 
+# reading profit value in row and appending it to trades
 for row in reader:
+    # here i converted to float data type for calculation
     row["profit"] = float(row["profit"])
     trades.append(row)
 print("total trades: " , total_trades)
 
+
+# coounting winning and losing trades
 for trade in trades:
     total_trades += trade["profit"]
 
