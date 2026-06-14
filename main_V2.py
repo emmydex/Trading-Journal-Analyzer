dc81b58d2c870d486c756622a1976eb313e00931
oluwafemi 🍀 <39830671+emmydex@users.noreply.github.com>
2026-06-14 15:54:30 -0400

    added output

--- main_V2.py
+++ main_V2.py
@@ -12,6 +12,11 @@ with open("journal.csv", "r") as file:
 total_trades = len(trades)
 print("total_trades:", total_trades)
 
+# empty file handling error
+if total_trades == 0:
+    print("no trades found")
+    exit()
+
 # naming variables
 
 winning_trades = 0
@@ -47,4 +52,19 @@ for trade in trades:
 win_rate = (winning_trades / total_trades) * 100
 average_profit = (total_profit / total_trades)
 
+print("\n======================")
+print("Trading Report")
+print("======================\n")
+print("Total Trades:", total_trades)
+print("Winning Trades:", winning_trades)
+print("Losing Trades:", losing_trades)
+print(f"Total profit: ${total_profit:.2f}")
+print(f"win rate: {win_rate:.2f}%")
+print(f"average profit: ${average_profit:.2f}")
+print("\nBest Trade:")
+print("pair:", best_trade['pair'])
+print(f"profit: ${best_trade["profit"]:.2f}")
 
+print("\nworst trade: ")
+print("pair:", worst_trade["pair"])
+print(f"profit: ${worst_trade["profit"]:.2f}")
