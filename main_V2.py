6a1cd0a3564a72a0e2dc56e78a907b2a559dda84
oluwafemi 🍀 <39830671+emmydex@users.noreply.github.com>
2026-06-14 15:11:16 -0400

    a 2nd version adding best trade and worse trade

---
+++ main_V2.py
@@ -0,0 +1,33 @@
+import csv
+
+
+trades = []
+with open("journal.csv", "r") as file:
+    reader = csv.DictReader(file)
+
+    for row in reader:
+        row['profit']= float(row["profit"])
+        trades.append(row)
+
+total_trades = len(trades)
+print("total_trades:", total_trades)
+
+winnig_trades = 0
+losing_trades = 0
+total_trades = 0
+best_trade = [0]
+worst_trade = [0]
+
+for trade in trades:
+    total_trades += trade["profit"]
+
+    if trade["profit"] > 0:
+        winning_trades += 1
+    else:
+        losing_trades += 1
+
+    if trade["profit"] > best_trade["profit"]:
+        best_trade = trade
+
+    if trade["profit"] < worst_trade["profit"]:
+        worst_trade = trade
