2d097f61db91a81fa130b27a8fcefcb34ae5217b
oluwafemi 🍀 <39830671+emmydex@users.noreply.github.com>
2026-06-17 19:42:21 -0400

    starting V3 which is going to feature profit factor

---
+++ main_V3.py
@@ -0,0 +1,99 @@
+import csv
+
+ # This script reads a CSV file named "journal.csv" containing trade data
+ 
+trades = []
+
+# The CSV file is expected to have a column named "profit" which contains the profit or loss for each trade.
+with open("journal.csv", "r") as file:
+    reader = csv.DictReader(file)
+
+    for row in reader:
+        # to avoid python reading the pairs as diffrent keys i used the .upper() to capitalize all
+        row["pair"] = row["pair"].upper()
+
+        # converting to a floating value
+        row['profit']= float(row["profit"])
+        trades.append(row)
+
+total_trades = len(trades)
+
+
+# empty file handling error
+if total_trades == 0:
+    print("no trades found")
+    exit()
+
+# naming variables
+
+winning_trades = 0
+losing_trades = 0
+total_profit = 0
+best_trade = trades[0]
+worst_trade = trades[0]
+
+# creating an empty dictionary for the variable
+pair_counts = {}
+
+# Loop through each trade in the list of trades and calculate the total profit
+for trade in trades:
+    total_profit += trade["profit"]
+
+    if trade["profit"] > 0:
+        winning_trades += 1
+    else:
+        losing_trades += 1
+
+# added statements for best trades and worst trades
+    if trade["profit"] > best_trade["profit"]:
+        best_trade = trade
+
+    if trade["profit"] < worst_trade["profit"]:
+        worst_trade = trade
+
+    # adds pair to the dictionary else if pair already exist in the dictionary it is added to the existing one
+    pair = trade["pair"]
+    # is pair in the dictionary variable if not add to the dictionary
+    if pair not in pair_counts:
+        pair_counts[pair] = 1
+    else:
+        # else add to existing 
+        pair_counts[pair] += 1
+
+
+     # getting the first pair
+    most_traded_pair = list(pair_counts.keys())[0]
+
+    #looping through the dictionary
+    for pair in pair_counts:
+
+        # comparing counts
+        if pair_counts[pair] > pair_counts[most_traded_pair]:
+            most_traded_pair = pair
+
+#  calculating win rate and average profit
+win_rate = (winning_trades / total_trades) * 100
+average_profit = (total_profit / total_trades)
+
+
+
+
+print("\n======================")
+print("Trading Report")
+print("======================\n")
+print("total_trades:", total_trades)
+print("Winning Trades:", winning_trades)
+print("Losing Trades:", losing_trades)
+print(f"Total profit: ${total_profit:.2f}")
+print(f"win rate: {win_rate:.2f}%")
+print(f"average profit: ${average_profit:.2f}")
+print("\nBest Trade:")
+print("pair:", best_trade['pair'])
+print(f"profit: ${best_trade['profit']:.2f}")
+
+print("\nworst trade: ")
+print("pair:", worst_trade["pair"])
+print(f"profit: ${worst_trade['profit']:.2f}")
+
+print("\nMost Traded pair:", most_traded_pair)
+print("Trades:", pair_counts[most_traded_pair])
