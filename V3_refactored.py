ba2a41730685c76127329035da46092eb7f3d47c
oluwafemi 🍀 <39830671+emmydex@users.noreply.github.com>
2026-06-23 11:33:56 -0400

    created a load_trades function

---
+++ V3_refactored.py
@@ -0,0 +1,17 @@
+import csv
+
+def load_trades(filename):
+
+    trades = []
+
+    with open(filename, "r") as file:
+        reader = csv.DictReader(file)
+
+        for row in reader:
+            row["pair"] = row["pair"].upper()
+            row["profit"] = float(row["profit"])
+            trades.append(row)
+
+    return trades
+
+trades = load_trades("journal.csv")
