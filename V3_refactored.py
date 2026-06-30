d2db4eaf7d707df1e788bb4bce5791aff3c31c92
oluwafemi 🍀 <39830671+emmydex@users.noreply.github.com>
2026-06-29 20:15:11 -0400

    finally

--- V3_refactored.py
+++ V3_refactored.py
@@ -122,9 +122,9 @@ def pair_performance(trades):
         profit = trade["pair"]
 
         if profit not in pair_profit :
-            pair_profit[pair] = pair_profit["profit"]
+            pair_profit[pair] = trade["profit"]
         else:
-            pair_profit[pair] += pair_profit["profit"]
+            pair_profit[pair] += trade["profit"]
        
 
     most_traded_pair = list(pair_counts.keys())[0]
