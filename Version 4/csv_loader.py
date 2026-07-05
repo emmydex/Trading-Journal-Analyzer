88e03039d4c5bbf135d88fea5a897e83f670949a
oluwafemi 🍀 <39830671+emmydex@users.noreply.github.com>
2026-07-04 22:15:40 -0400

    okay

---
+++ Version 4/csv_loader.py
@@ -0,0 +1,16 @@
+import csv
+
+# a function for loading and reading the csv file
+def load_trades(filename):
+
+    trades = []
+
+    with open(filename, "r") as file:
+        reader = csv.DictReader(file)
+
+        for row in reader:
+            row["pair"] = row["pair"].upper()
+            row["profit"] = float(row["profit"])
+            trades.append(row)
+
+    return trades
