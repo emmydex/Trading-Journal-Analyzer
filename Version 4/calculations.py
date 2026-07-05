44285b80d806fae740353fbf0b6d6d0d9a3b5318
oluwafemi 🍀 <39830671+emmydex@users.noreply.github.com>
2026-07-04 22:43:02 -0400

    MOVING

---
+++ Version 4/calculations.py
@@ -0,0 +1,101 @@
+
+# function for calculating metrics
+def calculate_metrics(trades):
+
+    winning_trades = 0
+    losing_trades = 0
+    total_profit = 0
+    breakeven_trades = 0
+    best_trade = trades[0]
+    worst_trade = trades[0]
+    gross_profit = 0
+    gross_loss = 0
+    current_win_streak = 0
+    current_loss_streak = 0
+    longest_win_streak =0
+    longest_loss_streak = 0
+    total_winning_profit = 0
+    total_losing_profit = 0
+
+
+    for trade  in trades:
+        if trade["profit"] > 0:
+            winning_trades +=1
+
+        # this is better if we factor in a breakeven trade
+        #this handles a losing trade and a breakeven trade (profit == 0) would not be counted
+        elif trade["profit"] < 0:
+            losing_trades += 1
+        
+        # added a variable for break even trades
+        else:
+            breakeven_trades += 1
+
+        #if trade["profit"] < 0:
+        #    losing_trades += 1
+       # there is no way best_trade and worst_trade are the same results
+
+        # NOTE:best_trade = trade ; makes the variable point to a diffrent dictionary.
+        # while best_trade["profit"] = trade["profit"] changes the contents of the current dictionary
+
+        if trade["profit"] > best_trade["profit"]:
+            
+            best_trade = trade
+
+        if trade["profit"] < worst_trade["profit"]:
+    
+            worst_trade = trade
+
+        if trade["profit"] > 0:
+            gross_profit += trade["profit"]
+
+            current_win_streak += 1
+            current_loss_streak = 0
+
+            total_winning_profit += trade["profit"]
+            
+        if current_win_streak > longest_win_streak:
+            longest_win_streak = current_win_streak
+            
+        elif trade["profit"] < 0 :
+            current_loss_streak += 1
+            current_win_streak = 0
+            gross_loss += abs(trade["profit"])
+            total_losing_profit += trade["profit"]
+
+        if current_loss_streak > longest_loss_streak:
+            longest_loss_streak = current_loss_streak
+        
+
+        total_profit += trade["profit"]
+
+    total_trades = len(trades)
+    
+    win_rate = (winning_trades / total_trades)* 100
+    average_profit = (total_profit / total_trades)
+    profit_factor = gross_profit/gross_loss
+    average_win = total_winning_profit/winning_trades
+    average_loss = total_losing_profit/losing_trades
+
+    return {
+        "total_trades": total_trades,
+        "winning_trades" : winning_trades,
+        "losing_trades" : losing_trades,
+        "total_profit" : total_profit,
+        "best_trade" : best_trade,
+        "worst_trade" : worst_trade,
+        'gross_loss' : gross_loss,
+        "gross_profit" : gross_profit,
+        "current_win_streak" : current_win_streak,
+        "current_loss_streak": current_loss_streak,
+        "longest_win_streak" : longest_win_streak,
+        "longest_loss_streak" : longest_loss_streak,
+        "total_winning_profit" : total_winning_profit,
+        "total_losing_profit" : total_losing_profit,
+        "breakeven_trades" : breakeven_trades,
+        "win_rate" : win_rate,
+        "average_profit" : average_profit,
+        "profit_factor" : profit_factor,
+        "average_win" : average_win,
+        "average_loss" : average_loss
+    }
