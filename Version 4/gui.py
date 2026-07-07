8040383d68b885af3fd381aaa09bf040aa904aa3
oluwafemi 🍀 <39830671+emmydex@users.noreply.github.com>
2026-07-07 17:48:45 -0400

    hello

--- Version 4/gui.py
+++ Version 4/gui.py
@@ -3,6 +3,8 @@ import tkinter as tk
 window = tk.Tk()
 window.title("Trading Journal Analyzer")
 window.geometry("800x500")
-label = tk.Label(window, text="OLUWA DID")
+label = tk.Label(window, text="My First Trading Journal")
 label.pack()
+
+
 window.mainloop()
