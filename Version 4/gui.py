064632dbaad29f89e0e9abaa89081362633d63cf
oluwafemi 🍀 <39830671+emmydex@users.noreply.github.com>
2026-07-09 09:48:16 -0400

    testing tkinter

--- Version 4/gui.py
+++ Version 4/gui.py
@@ -1,12 +1,11 @@
 import tkinter as tk
+# running a demo
 
 window = tk.Tk()
 window.title("Trading Journal Analyzer")
-window.geometry("800x500")
-label = tk.Label(window, text="My First Trading Journal")
-label.pack()
-another_label =tk.Label(window, text='lets Create, oluwafemi')
-another_label.pack()
+window.geometry("500x300")
+label = tk.Label(window, text="")
+
 
 
 window.mainloop()
