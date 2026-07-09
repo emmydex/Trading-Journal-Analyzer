87b8e17fc510a5225c46304c6c55bd12a3599ac9
oluwafemi 🍀 <39830671+emmydex@users.noreply.github.com>
2026-07-09 09:53:05 -0400

    created a function

--- Version 4/gui.py
+++ Version 4/gui.py
@@ -4,8 +4,13 @@ import tkinter as tk
 window = tk.Tk()
 window.title("Trading Journal Analyzer")
 window.geometry("500x300")
-label = tk.Label(window, text="")
+ #creating a label status_label
+status_label = tk.Label(window, text="Status: Not clicked")
 
+status_label.pack(pady=20)
 
+#created a function button_clicked
+def button_clicked():
+    pass
 
 window.mainloop()
