dc7c74ee1d24d9b58e712fdcc483f444fe3daf24
oluwafemi 🍀 <39830671+emmydex@users.noreply.github.com>
2026-07-10 19:56:19 -0400

    creating  frame structure

--- Version 4/gui.py
+++ Version 4/gui.py
@@ -2,26 +2,8 @@ import tkinter as tk
 # running a demo
 
 window = tk.Tk()
-window.title("Trading Journal Analyzer")
-window.geometry("500x300")
- #creating a label status_label
-status_label = tk.Label(window, text="Status: Not clicked")
+window.title("Trading Journal Analyzer V4")
+window.geometry("900x600")
+ #creating the frame structure
 
-status_label.pack(pady=20)
-
-#created a function button_clicked which changes the label when clicked
-def button_clicked():
-    status_label.config(
-        text="Status: Button Clicked!"
-    )
-
-
-button = tk.Button(
-    window,
-    text="Click Me",
-    #passed the button_clicked function to button's command parameter
-    command=button_clicked
-)
-
-button.pack()
 window.mainloop()
