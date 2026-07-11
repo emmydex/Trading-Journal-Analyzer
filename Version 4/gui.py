e5c8ee9be6e57fb21a4f2323ccdab89b981d5205
oluwafemi 🍀 <39830671+emmydex@users.noreply.github.com>
2026-07-10 20:00:43 -0400

    added statistics frame

--- Version 4/gui.py
+++ Version 4/gui.py
@@ -6,4 +6,12 @@ window.title("Trading Journal Analyzer V4")
 window.geometry("900x600")
  #creating the frame structure
 
+ #Button frame
+button_frame = tk.Frame(window)
+button_frame.pack()
+
+# Statistics Frame
+stats_frame = tk.Frame(window)
+stats_frame.pack()
+
 window.mainloop()
